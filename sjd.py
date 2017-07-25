01fdeee02875dcc5753011a362706afe625bb3d8
程刚 <chenggang0815@users.noreply.github.com>
2017-07-25 13:43:52 +0800

    Update sjd.py

--- sjd.py
+++ sjd.py
@@ -1,78 +1,130 @@
+print('hello')
+
 # coding: utf-8
 import re
 import requests
 from bs4 import BeautifulSoup as bs
-import re
 
-head = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:51.0) Gecko/20100101 Firefox/51.0'}
+head = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0'}
 
-url = 'https://tieba.baidu.com/f?ie=utf-8&kw=%E6%89%8B%E6%9C%BA%E8%B4%B7&fr=search'
+'''
+url = 'https://tieba.baidu.com/f?kw=%E6%89%8B%E6%9C%BA%E8%B4%B7&ie=utf-8&pn=50'
 
 res = requests.get(url,headers=head)
 
 res2 = res.text.replace('<!--','')
 res3 = res2.replace('-->','')
 
-soup = bs(res3,'lxml')
+soup = bs(res3,'lxml')'''
 #print(soup.body.a)
 
-#url = 'https://tieba.baidu.com/f?kw=%E6%89%8B%E6%9C%BA%E8%B4%B7&ie=utf-8&pn=50'
+base_url = 'https://tieba.baidu.com/f?kw=%E6%89%8B%E6%9C%BA%E8%B4%B7&ie=utf-8'
+deep =1
+url_list = []
+# 将所有需要爬去的url存入列表
+for i in range(0, deep):
+    url_list.append(base_url + '&pn=' + str(50 * i))
+    print('所有的网页已经下载到本地！ 开始筛选信息。。。。')
+
+res=[];res2=[];res3=[]
+for url in url_list:
+    res.append(requests.get(url,headers=head))
+
+print(len(res))
+for html in res:
+    res2.append(html.text.replace('<!--',''))
+
+for html in res2:
+    res3.append(html.replace('-->',''))
+
+print(len(res2))
+print(len(res3))
+
+soup=[]
+for i in res3:
+    soup.append(bs(i,'lxml'))
+
+title = [] #标题
+author = [] # 作者
+time=[] #发帖时间
+reply_num=[]#帖子回复数
+tiezi_url =[]#帖子ID
+for s in soup:
+    print('title-----------------------------------')
+    a =s.find_all('a',class_='j_th_tit ')
+    for i in a:
+        title.append(i.string)
 
 
-#for i in page:
- #   url ='https://www.aqistudy.cn/historydata/daydata.php?city=%E4%B8%8A%E6%B5%B7&month='+i
-  #  print(i)
-   # res = requests.get(url,headers=head)
 
-#soup = bs(res.text)
+    print('author-------------------------------')
+    b = s.find_all('span', title=re.compile('(主题作者.*)'))
+    for i in b:
+        author.append(i.get_text())
 
-title = []
 
-print('title-----------------------------------')
-a =soup.find_all('a',class_='j_th_tit ')
-print(type(a))
-for i in a:
-   # print(i.string)
-    title.append(i.string)
+    print('time---------------------------------')
+    c = s.find_all('span',class_='pull-right is_show_create_time')
+    for i in c:
+        time.append(i.get_text())
 
-author = []
-print('author-------------------------------')
-b = soup.find_all('span', class_='tb_icon_author ')
-print(type(b))
-for i in b:
-    #print(i.get_text())
-    author.append(i.get_text())
+    print('reply_num---------------------------------')
+    d= s.find_all('span',class_='threadlist_rep_num center_text')
+    for i in d:
+        reply_num.append(i.get_text())
 
-time=[]
-print('time---------------------------------')
-c = soup.find_all('span',class_='pull-right is_show_create_time')
-for i in c:
-    #print(i.get_text())
-    time.append(i.get_text())
+    print('title_id----------------------------------')
+    e = s.find_all('a', class_='j_th_tit ')
+    for i in e:
+        tiezi_url.append('https://tieba.baidu.com'+i.get('href'))
 
-reply_num=[]
-print('reply_num---------------------------------')
-d= soup.find_all('span',class_='threadlist_rep_num center_text')
-for i in d:
-    #print(i.get_text())
-    reply_num.append(i.get_text())
+#    for  i in tiezi_url:
+#        print(i)
 
-print(len(title))
-print(len(time))
-print(len(author))
-print(len(reply_num))
-'''with open('shoujidai.csv','a') as f:
-    for y in range(50):
+print('开始爬取帖子内容')
+# 爬取每个帖子里的回复
+
+soup2=[]
+res4=[]
+for url in tiezi_url:
+    res4.append(requests.get(url, headers=head).text)
+    #print('解析帖子url')
+    print(url)
+
+
+
+for i in res4:
+    soup2.append(bs(i,'lxml'))
+    #print('添加帖子soup')
+
+title2=[]
+for s in soup2:
+#帖子标题
+    title2.append(s.find_all('h3'))
+    print(s.find_all('h3')[0]['title'])
+    print(type(s.find_all('h3')))
+    #print(s.html)
+
+for i in title2:
+    print(re.findall('">(.*)</h3>',i))
+
+print('Done!')
+
+
+
+#print(len(title))
+#print(len(time))
+#print(len(author))
+#print(len(reply_num))
+#print(len(tiezi_url))
+'''
+with open('shoujidai.csv','a') as f:
+    for y in range(150):
         sjd_data = '%s; %s; %s; %s' % (title[y],author[y],time[y],reply_num[y])
         f.writelines(sjd_data + '\n')
-print('loading...')'''
+        print('loading...')'''
 
 
-#print(type(res.text))
 
-#print(soup.title)
-#print(soup.p)
-#print(soup.a)
 
-#print(soup.body)
 
