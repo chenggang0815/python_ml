551ea8968c78a4d4f8a7f1a287aa01ef0c410cf9
chenggang0815 <954036868@qq.com>
2020-10-04 10:18:19 +0800

    -

--- python_crawler/craw_sjd_threading.py
+++ python_crawler/craw_sjd_threading.py
@@ -10,7 +10,6 @@ import queue
 from lxml import etree
 
 
-
 class ThreadUrl(threading.Thread):
     def __init__(self, queue):
         threading.Thread.__init__(self)
@@ -19,18 +18,16 @@ class ThreadUrl(threading.Thread):
     def run(self):
         while True:
             url = self.queue.get()
-            res=requests.get(url,headers=head).text
-            res=res.replace('<!--', '')
-            res=res.replace('-->', '')
-            soup=bs(res,'lxml')
+            res = requests.get(url, headers=head).text
+            res = res.replace('<!--', '')
+            res = res.replace('-->', '')
+            soup = bs(res,'lxml')
             a = soup.find_all('a', class_='j_th_tit ')
             for i in a:
                 title.append(i.string)
             id = soup.find_all('a', class_='j_th_tit ')
             for i in id:
                 tiezi_url.append('https://tieba.baidu.com' + i.get('href'))
-
-
 
             print(self.getName(),":",url)
             self.queue.task_done()
@@ -57,10 +54,9 @@ class ThreadUrl2(threading.Thread):
             url = self.queue.get()
             get_all_comment(url)
 
-
-
             print(self.getName(),":",url)
             self.queue.task_done()
+
 
 def main2():
     # spawn a pool of threads, and pass them queue instance
@@ -73,25 +69,26 @@ def main2():
     q2.join()
 
 
-def openlink(url):
-    maxTryNum = 10
-    for tries in range(maxTryNum):
+def open_link(url):
+    try_num = 10
+    for tries in range(try_num):
         try:
-            res =requests.get(url, headers=head,timeout=100).text
+            res =requests.get(url, headers=head, timeout=100).text
             time.sleep(0.2)
             break
         except:
-            if tries < (maxTryNum - 1):
+            if tries < (try_num - 1):
                 continue
             else:
-                print("Has tried %d times to access url %s, all failed!", maxTryNum, url)
+                print("Has tried %d times to access url %s, all failed!", try_num, url)
                 break
     return res
 
+
 def get_page_num(url):
-    res = openlink(url)
-    soup = bs(res,'lxml')
-    if soup.find('title').get_text()=='贴吧404' or soup.find('title').get_text()=='404 Not Found':
+    res = open_link(url)
+    soup = bs(res, 'lxml')
+    if soup.find('title').get_text() == '贴吧404' or soup.find('title').get_text() == '404 Not Found':
         page_num = 0
         return page_num
     else:
@@ -105,6 +102,7 @@ def get_page_num(url):
             page_num = int(a[0])
             return page_num
 
+
 def get_all_comment(url):
     url_list = []; res = [];soup = [];comment = [];user_name=[];comment_time=[];html=[];title=[]
     for i in range(1, get_page_num(url)+1):
@@ -112,7 +110,7 @@ def get_all_comment(url):
             continue
         url_list.append(url + '?&pn=' + str(i))
     for i in url_list:
-        res.append(openlink(i))
+        res.append(open_link(i))
     for i in res:
         soup.append(bs(i,'lxml'))
         html.append(etree.HTML(i))
@@ -124,9 +122,9 @@ def get_all_comment(url):
     for s in soup:
         comment.append(s.find_all('div',class_='d_post_content j_d_post_content '))
         user_name.append(s.find_all('img', username=re.compile('\S')))
-    n=0
+    n = 0
     for i in comment:
-        n=n+1
+        n = n+1
         for j in range(len(i)):
             all_comment.append(i[j].get_text())
             #all_title.append(title[n-1])
@@ -135,8 +133,9 @@ def get_all_comment(url):
         for j in range(len(i)):
             all_user_name.append(i[j].get('username'))
 
-if __name__=='__main__':
-    starttime = datetime.datetime.now()
+
+if __name__ == '__main__':
+    start_time = datetime.datetime.now()
     head = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0'}
     base_url = 'https://tieba.baidu.com/f?kw=手机贷&ie=utf-8'
     q = queue.Queue()  # 存放每个列表页的URL
@@ -145,11 +144,11 @@ if __name__=='__main__':
     all_user_name = []
     all_comment_time = []
     all_title = []
-    title=[]
+    title = []
 
-    base_url_list=[]
-    tiezi_url=[]
-    num_page=120
+    base_url_list = []
+    tiezi_url = []
+    num_page = 120
     for i in range(0, num_page):
         base_url_list.append(base_url+ '&pn=' + str(50 * i))
         print('https://tieba.baidu.com/f?kw=%E9%80%BE%E6%9C%9F&ie=utf-8'
@@ -165,8 +164,8 @@ if __name__=='__main__':
     list_2 = [all_comment, all_comment_time, all_user_name]
     data2 = pd.DataFrame(list_2)
     data2 = data2.T
-    data2.columns = [ 'reply_content', 'reply_time', 'author']
+    data2.columns = ['reply_content', 'reply_time', 'author']
     print('开始导出回复数据！')
     data2.to_csv('手机贷吧帖子回复.csv')
-    endtime = datetime.datetime.now()
-    print('共消耗时间：%d秒' % ((endtime - starttime).seconds))
+    end_time = datetime.datetime.now()
+    print('共消耗时间：%d秒' % (end_time - start_time).seconds)
